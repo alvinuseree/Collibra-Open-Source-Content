c2bfc5ff1fd4c6fd428b0b3b702f1fb8e1077e6a
Alvin Useree <41648377+alvinuseree@users.noreply.github.com>
2022-10-17 09:38:02 +0100

    First Commit

--- Workflows/Groovy Code/Tracking Collibra Synchronisation Metadata.groovy
+++ Workflows/Groovy Code/Tracking Collibra Synchronisation Metadata.groovy
@@ -1,1 +1,19 @@
-place
+//This code enables the tracking of a Sync Job
+import com.collibra.dgc.core.api.dto.job.FindJobsRequest
+import com.collibra.dgc.core.api.model.job.JobState
+
+//Id of the Sync Job:
+def syncName = "Synchronization of batch for id: f14cfeb2-f1e1-402b-956e-07a7c0e46db4"
+
+//Begin by finding the UUID of any relevant Sync Jobs:
+def jobsOfInterest = jobApi.findJobs(FindJobsRequest.builder()
+                                            .types(["IMPORT"])
+                                            .states([JobState.COMPLETED])
+                                            .name(syncName)
+                                            .build()).getResults()
+
+//For every job of interest log the job message and the import summary of the job
+jobsOfInterest.each{
+    loggerApi.info(it.getMessage())
+    loggerApi.info("${importerApi.getImportJobSummary(it.getId())}")  
+}
